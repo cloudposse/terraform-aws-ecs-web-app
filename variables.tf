de7228f201c24bb6c695c0a7875ca322424716ec
Sarkis Varozian <svarozian@gmail.com>
2018-07-02 17:35:28 -0700

    changes from CR

--- variables.tf
+++ variables.tf
@@ -85,57 +85,57 @@ variable "sns_topic_name" {
   default     = ""
 }
 
-variable "alb_alarms_enabled" {
+variable "alb_target_group_alarms_enabled" {
   type        = "string"
   description = "A boolean to enable/disable CloudWatch Alarms for ALB Target metrics."
   default     = "false"
 }
 
-variable "alb_alarms_3xx_threshold" {
+variable "alb_target_group_alarms_3xx_threshold" {
   type        = "string"
   description = "The maximum number of 3XX HTTPCodes in a given period for ECS Service."
   default     = "25"
 }
 
-variable "alb_alarms_4xx_threshold" {
+variable "alb_target_group_alarms_4xx_threshold" {
   type        = "string"
   description = "The maximum number of 4XX HTTPCodes in a given period for ECS Service."
   default     = "25"
 }
 
-variable "alb_alarms_5xx_threshold" {
+variable "alb_target_group_alarms_5xx_threshold" {
   type        = "string"
   description = "The maximum number of 5XX HTTPCodes in a given period for ECS Service."
   default     = "25"
 }
 
-variable "alb_alarms_response_time_threshold" {
+variable "alb_target_group_alarms_response_time_threshold" {
   type        = "string"
   description = "The maximum ALB Target Group response time."
   default     = "0.5"
 }
 
-variable "alb_alarms_period" {
+variable "alb_target_group_alarms_period" {
   type        = "string"
   description = "The period (in seconds) to analyze for ALB CloudWatch Alarms."
   default     = "300"
 }
 
-variable "alb_alarms_evaluation_periods" {
+variable "alb_target_group_alarms_evaluation_periods" {
   type        = "string"
   description = "The number of periods to analyze for ALB CloudWatch Alarms."
   default     = "1"
 }
 
-variable "alb_alarms_alb_name" {
+variable "alb_name" {
   type        = "string"
-  description = "Name of the ALB used for CloudWatch Dimensions."
+  description = "Name of the ALB for the Target Group."
   default     = ""
 }
 
-variable "alb_alarms_alb_arn_suffix" {
+variable "alb_arn_suffix" {
   type        = "string"
-  description = "ARN suffix of the ALB used for CloudWatch Dimensions."
+  description = "ARN suffix of the ALB for the Target Group."
   default     = ""
 }
 
@@ -183,13 +183,13 @@ variable "ecs_alarms_enabled" {
   default     = "false"
 }
 
-variable "ecs_alarms_cpu_util_threshold" {
+variable "ecs_alarms_cpu_utilization_threshold" {
   type        = "string"
   description = "The max percentage of CPU usage for ECS Service."
   default     = "80"
 }
 
-variable "ecs_alarms_mem_util_threshold" {
+variable "ecs_alarms_mem_utilization_threshold" {
   type        = "string"
   description = "The max percentage of Memory usage for ECS Service."
   default     = "80"
