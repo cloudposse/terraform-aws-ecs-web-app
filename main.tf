ee8d28814e92c683f5345abb8b921a5e93c1e846
Erik Osterman <erik@cloudposse.com>
2019-01-10 23:08:08 -0800

    add output

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "container_definition" {
 }
 
 module "ecs_alb_service_task" {
-  source                    = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task.git?ref=tags/0.6.0"
+  source                    = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task.git?ref=add-outputs"
   name                      = "${var.name}"
   namespace                 = "${var.namespace}"
   stage                     = "${var.stage}"
