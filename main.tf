f8aea04cdfac5d1f2b33b4524b22d3f5f397562b
Erik Osterman <erik@cloudposse.com>
2019-01-13 23:07:57 -0800

    bump modules

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "ecs_alb_service_task" {
 
 module "ecs_codepipeline" {
   enabled             = "${var.codepipeline_enabled}"
-  source              = "git::https://github.com/cloudposse/terraform-aws-ecs-codepipeline.git?ref=add-params"
+  source              = "git::https://github.com/cloudposse/terraform-aws-ecs-codepipeline.git?ref=add-webhook-support"
   name                = "${var.name}"
   namespace           = "${var.namespace}"
   stage               = "${var.stage}"
