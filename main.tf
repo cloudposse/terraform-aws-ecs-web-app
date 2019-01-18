ba5070b6970c9b1eec758acb0c1c06d44280aa39
Erik Osterman <erik@cloudposse.com>
2019-01-17 21:01:25 -0800

    Pin

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "ecs_alb_service_task" {
 
 module "ecs_codepipeline" {
   enabled               = "${var.codepipeline_enabled}"
-  source                = "git::https://github.com/cloudposse/terraform-aws-ecs-codepipeline.git?ref=use-join-splat"
+  source                = "git::https://github.com/cloudposse/terraform-aws-ecs-codepipeline.git?ref=tags/0.4.2"
   name                  = "${var.name}"
   namespace             = "${var.namespace}"
   stage                 = "${var.stage}"
