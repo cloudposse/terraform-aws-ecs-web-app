67a9da58369a0a9eee68eb348e480ca09821d39b
Joe Niland <joe@originalmind.com.au>
2020-05-25 14:11:38 +1000

    fix output name for ecr.repository_url

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,13 @@ output "ecr_registry_id" {
 }
 
 output "ecr_registry_url" {
-  value       = module.ecr.registry_url
-  description = "Registry URL"
+  value       = module.ecr.repository_url
+  description = "Repository URL"
+}
+
+output "ecr_repository_url" {
+  value       = module.ecr.repository_url
+  description = "Repository URL"
 }
 
 output "ecr_repository_name" {
