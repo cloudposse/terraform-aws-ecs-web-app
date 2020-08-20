0599fb9d0b3c759a384290ef08998caf251f1f0f
Frank Klaassen <frank@cloudright.nl>
2020-08-20 10:22:01 +0200

    update codepipeline

--- main.tf
+++ main.tf
@@ -162,7 +162,7 @@ module "ecs_alb_service_task" {
 
 module "ecs_codepipeline" {
   enabled               = var.codepipeline_enabled
-  source                = "git::https://github.com/cloudposse/terraform-aws-ecs-codepipeline.git?ref=tags/0.14.1"
+  source                = "git::https://github.com/cloudposse/terraform-aws-ecs-codepipeline.git?ref=tags/0.16.0"
   name                  = var.name
   namespace             = var.namespace
   stage                 = var.stage
