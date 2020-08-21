6f2ddac81a175cb6d55cac145294a789ee9979cf
Joe Niland <joe@originalmind.com.au>
2020-08-21 11:49:21 +1000

    Increase route create/delete timeouts in examples

--- examples/with_cognito_authentication/main.tf
+++ examples/with_cognito_authentication/main.tf
@@ -18,17 +18,19 @@ locals {
 }
 
 module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.27.0"
-  availability_zones   = local.availability_zones
-  namespace            = var.namespace
-  stage                = var.stage
-  name                 = var.name
-  region               = var.region
-  vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = true
-  nat_instance_enabled = false
+  source                   = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.27.0"
+  availability_zones       = local.availability_zones
+  namespace                = var.namespace
+  stage                    = var.stage
+  name                     = var.name
+  region                   = var.region
+  vpc_id                   = module.vpc.vpc_id
+  igw_id                   = module.vpc.igw_id
+  cidr_block               = module.vpc.vpc_cidr_block
+  nat_gateway_enabled      = true
+  nat_instance_enabled     = false
+  aws_route_create_timeout = "5m"
+  aws_route_delete_timeout = "10m"
 }
 
 module "alb" {
