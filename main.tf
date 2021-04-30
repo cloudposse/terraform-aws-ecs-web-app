9c0c83a7fb982ee6ff73046abea6299ab683b76e
Joe Niland <joe@originalmind.com.au>
2021-04-30 11:44:55 +1000

    bump version of codepipeline module

--- main.tf
+++ main.tf
@@ -161,7 +161,7 @@ module "ecs_alb_service_task" {
 module "ecs_codepipeline" {
   enabled = var.codepipeline_enabled
   source  = "cloudposse/ecs-codepipeline/aws"
-  version = "0.26.0"
+  version = "0.27.0"
 
   region                      = coalesce(var.region, data.aws_region.current.name)
   github_oauth_token          = var.github_oauth_token
