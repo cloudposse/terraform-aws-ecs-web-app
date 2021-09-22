02281f0aab2307165705ed1171b852358ef33201
Ryan Ryke <ryan@itsacloudlife.com>
2021-09-22 06:43:40 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -146,67 +146,67 @@ output "cloudwatch_log_group_name" {
   value       = join("", aws_cloudwatch_log_group.app.*.name)
 }
 
-output "codebuild" {
-  description = "All outputs from `module.ecs_codepipeline`"
-  value       = module.ecs_codepipeline
-  sensitive   = true
-}
+# output "codebuild" {
+#   description = "All outputs from `module.ecs_codepipeline`"
+#   value       = module.ecs_codepipeline
+#   sensitive   = true
+# }
 
-output "codebuild_project_name" {
-  description = "CodeBuild project name"
-  value       = module.ecs_codepipeline.codebuild_project_name
-}
+# output "codebuild_project_name" {
+#   description = "CodeBuild project name"
+#   value       = module.ecs_codepipeline.codebuild_project_name
+# }
 
-output "codebuild_project_id" {
-  description = "CodeBuild project ID"
-  value       = module.ecs_codepipeline.codebuild_project_id
-}
+# output "codebuild_project_id" {
+#   description = "CodeBuild project ID"
+#   value       = module.ecs_codepipeline.codebuild_project_id
+# }
 
-output "codebuild_role_id" {
-  description = "CodeBuild IAM Role ID"
-  value       = module.ecs_codepipeline.codebuild_role_id
-}
+# output "codebuild_role_id" {
+#   description = "CodeBuild IAM Role ID"
+#   value       = module.ecs_codepipeline.codebuild_role_id
+# }
 
-output "codebuild_role_arn" {
-  description = "CodeBuild IAM Role ARN"
-  value       = module.ecs_codepipeline.codebuild_role_arn
-}
+# output "codebuild_role_arn" {
+#   description = "CodeBuild IAM Role ARN"
+#   value       = module.ecs_codepipeline.codebuild_role_arn
+# }
 
-output "codebuild_cache_bucket_name" {
-  description = "CodeBuild cache S3 bucket name"
-  value       = module.ecs_codepipeline.codebuild_cache_bucket_name
-}
+# output "codebuild_cache_bucket_name" {
+#   description = "CodeBuild cache S3 bucket name"
+#   value       = module.ecs_codepipeline.codebuild_cache_bucket_name
+# }
 
-output "codebuild_cache_bucket_arn" {
-  description = "CodeBuild cache S3 bucket ARN"
-  value       = module.ecs_codepipeline.codebuild_cache_bucket_arn
-}
+# output "codebuild_cache_bucket_arn" {
+#   description = "CodeBuild cache S3 bucket ARN"
+#   value       = module.ecs_codepipeline.codebuild_cache_bucket_arn
+# }
 
-output "codebuild_badge_url" {
-  description = "The URL of the build badge when badge_enabled is enabled"
-  value       = module.ecs_codepipeline.codebuild_badge_url
-}
+# output "codebuild_badge_url" {
+#   description = "The URL of the build badge when badge_enabled is enabled"
+#   value       = module.ecs_codepipeline.codebuild_badge_url
+# }
 
-output "codepipeline_id" {
-  description = "CodePipeline ID"
-  value       = module.ecs_codepipeline.codepipeline_id
-}
+# output "codepipeline_id" {
+#   description = "CodePipeline ID"
+#   value       = module.ecs_codepipeline.codepipeline_id
+# }
 
-output "codepipeline_arn" {
-  description = "CodePipeline ARN"
-  value       = module.ecs_codepipeline.codepipeline_arn
-}
+# output "codepipeline_arn" {
+#   description = "CodePipeline ARN"
+#   value       = module.ecs_codepipeline.codepipeline_arn
+# }
 
-output "codepipeline_webhook_id" {
-  description = "The CodePipeline webhook's ID"
-  value       = module.ecs_codepipeline.webhook_id
-}
+# output "codepipeline_webhook_id" {
+#   description = "The CodePipeline webhook's ID"
+#   value       = module.ecs_codepipeline.webhook_id
+# }
 
-output "codepipeline_webhook_url" {
-  description = "The CodePipeline webhook's URL. POST events to this endpoint to trigger the target"
-  value       = module.ecs_codepipeline.webhook_url
-  sensitive   = true
-}
+# output "codepipeline_webhook_url" {
+#   description = "The CodePipeline webhook's URL. POST events to this endpoint to trigger the target"
+#   value       = module.ecs_codepipeline.webhook_url
+#   sensitive   = true
+# }
 
 output "ecs_cloudwatch_autoscaling" {
   description = "All outputs from `module.ecs_cloudwatch_autoscaling`"
