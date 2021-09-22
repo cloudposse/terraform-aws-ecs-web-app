8e736245ffbf500e5d6c28f1da13feab38fa6e8f
Ryan Ryke <ryan@itsacloudlife.com>
2021-09-22 06:55:41 -0700

    Update extras.tf

--- extras.tf
+++ extras.tf
@@ -23,12 +23,12 @@ data "aws_iam_policy_document" "task_perms" { #UPDATE: adding some basic permiss
 }
 
 resource "aws_iam_policy" "task_perms" {
-  name   = module.web_app.ecs_service_name
+  name   = module.ecs_alb_service_task.service_name
   path   = "/"
   policy = data.aws_iam_policy_document.task_perms.json
 }
 
 resource "aws_iam_role_policy_attachment" "attach" {
-  role       = module.web_app.ecs_task_role_name
+  role       = module.ecs_alb_service_task.task_role_name
   policy_arn = aws_iam_policy.task_perms.arn
 }
