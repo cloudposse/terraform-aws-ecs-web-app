48a4ee852e3650e450bec7bb2dfb88218b17b9cb
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-03-29 08:17:53 +0000

    Auto Format

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ module "container_definition" {
   mount_points                 = var.mount_points
   container_depends_on         = local.container_depends_on
   repository_credentials       = var.container_repo_credentials
-  linux_parameters             = merge(
+  linux_parameters = merge(
     var.linux_parameters,
     var.exec_enabled ? { initProcessEnabled = true } : {}
   )
