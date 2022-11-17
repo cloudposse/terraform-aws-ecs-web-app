2a9f228eb3ce37387526f32c023b67b990a2201c
zaksamalik <sam@allref.io>
2022-11-16 23:02:15 -0500

    * add protocol and protocol_version variables

--- main.tf
+++ main.tf
@@ -24,8 +24,12 @@ module "alb_ingress" {
   source  = "cloudposse/alb-ingress/aws"
   version = "0.24.2"
 
-  vpc_id                           = var.vpc_id
-  port                             = var.container_port
+  vpc_id = var.vpc_id
+  port   = var.container_port
+
+  protocol         = var.alb_ingress_protocol
+  protocol_version = var.alb_ingerss_protocol_version
+
   health_check_path                = var.alb_ingress_healthcheck_path
   health_check_protocol            = var.alb_ingress_healthcheck_protocol
   health_check_healthy_threshold   = var.alb_ingress_health_check_healthy_threshold
