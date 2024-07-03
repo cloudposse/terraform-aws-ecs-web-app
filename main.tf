47757fa80c22d9e46f3768d65b20c9f80e2d8121
Igor Rodionov <goruha@gmail.com>
2024-07-03 18:25:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -241,7 +241,7 @@ module "ecs_codepipeline" {
 module "ecs_cloudwatch_autoscaling" {
   enabled               = var.autoscaling_enabled
   source                = "cloudposse/ecs-cloudwatch-autoscaling/aws"
-  version               = "0.7.3"
+  version               = "0.7.5"
   name                  = var.name
   namespace             = var.namespace
   stage                 = var.stage
